b3ecbbd9b2bbea8bc6973029c833f79046f8a838
AMOUSSA Soultone <amoussasoultone0@gmail.com>
2026-03-18 10:54:26 +0100

    models: ajout du modèle User

---
+++ backend/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Enum, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from database.db import Base
+import uuid
+import datetime
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String(255), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    role = Column(Enum('pmo', 'chef_projet', 'direction', 'consultant', name='user_role'), nullable=False, default='pmo')
+    created_at = Column(DateTime, default=datetime.datetime.now)
